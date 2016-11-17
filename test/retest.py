b5c2857178d7d4e38dfdbf81f92f1092edeeb707
wudan <wudan@shinezone.com>
2016-11-17 18:39:40 +0800

    + test

--- test/retest.py
+++ test/retest.py
@@ -8,6 +8,22 @@ def match_quote(str_to_match):
         print('match failed!')
 
 
+def array_constructor():
+    raw = [1, 2, 3]
+    print([x * 2 for x in raw])
+
+
+def set_minuse():
+    set1 = set([1, 2, 3])
+    set2 = set([2, 3, 4])
+    print(set1 - set2)
+    print(set1 | set2)
+    print(set1 & set2)
+
+
 if __name__ == '__main__':
     print(':"哈哈\'s科技时尚男装"')
     match_quote(':"哈哈\'s科技时尚男装"')
+
+    array_constructor()
+    set_minuse()
